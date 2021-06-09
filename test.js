2f19c5603475cb882cd63a1e506d87c7cb3741ae
predator <Ashwani@Predator>
2021-06-09 21:06:55 +0530

    create two js file one with loop to create randomNum and another is just DOM model

---
+++ test.js
@@ -0,0 +1,1 @@
+document.querySelectorAll("button",()=>{console.log("doin nothin")});
---
+++ test2.js
@@ -0,0 +1,5 @@
+for(let i=0; i<5; i++)
+{
+    let randomNum=Math.floor(Math.random()*5);
+    console.log(randomNum);
+}
